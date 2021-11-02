7d75f7f2d1ce78ff7e71b329f587dfd41f0e8b89
YOSHIDA Satoshi <shidocchi@gmail.com>
2021-11-02 14:45:37 +0100

    use setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="binsearch",
-    version="0.1.0",
-    author="shidocchi",
-    author_email="shidocchi@gmail.com",
-    description="binary search algorithm",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/shidocchi/binsearch",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
